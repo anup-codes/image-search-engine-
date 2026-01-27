20dbd600f55e04dae8fba9530df9324d61c171eb
anup-codes <anup2812007@gmail.com>
2026-01-28 02:55:41 +0530

    final image search

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const accesskey="W8uqEg4CjECKUbkjaXykiU9NOtANzpZ08S8pEuE2gQU";
 
-const searchForm = document.getElementById(".search-form");
-const searchBox = document.getElementById(".search-box");
-const searchResult = document.getElementById(".search-result");
-const showMoreBtn = document.getElementById(".show-more-btn");
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
 
 
 let keyword = "";
 let page = 1;
-async function searchImage(){
-  keyword = search.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}`;
+async function searchImages(){
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  if(page === 1 ){
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+
+  results.map((result) =>{
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  })
+  showMoreBtn.style.display = "block";
 }
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page=1;
+  searchImages();
+}) 
+
+showMoreBtn.addEventListener("click", ()=>{
+  page++;
+  searchImages();
+})
